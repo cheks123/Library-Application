c3f37c1ec05e04465b1c21a2c430284bd756d698
cheks123 <chekwubeutomi@gmail.com>
2022-05-21 00:38:03 -0700

    authors, books and index routes done

--- routes/books.js
+++ routes/books.js
@@ -4,6 +4,7 @@ const Book = require('../models/book');
 const Author = require('../models/author');
 const multer = require('multer');
 const path = require('path');
+const fs = require('fs');
 const uploadPath = path.join('public', Book.coverImageBasePath);
 const imageMimeType = ['images/jpeg', 'images/png', 'images/gif']
 const upload = multer({
@@ -18,7 +19,29 @@ const upload = multer({
 
 //All books route
 router.get('/', async (req, res)=>{
-    res.send("All books")
+    let query = Book.find();
+    if(req.query.title != null && req.query.title != ''){
+        query = query.regex('title', new RegExp(req.query.title, 'i'))
+    }
+    if(req.query.publishBefore != null && req.query.publishBefore != ''){
+        query = query.lte('publishDate', req.query.publishBefore);
+    }
+    if(req.query.publishAfter != null && req.query.publishAfter != ''){
+        query = query.gte('publishDate', req.query.publishAfter);
+    }
+
+    try{
+        const books = await query.exec()
+        res.render('books/index', {
+            books:books,
+            searchOptions: req.query
+        })
+
+    }
+    catch{
+        redirect('/')
+    }
+
     
 });
 
@@ -41,16 +64,16 @@ router.post('/', upload.single('cover'), async (req,res)=>{
         description: req.body.description
     })
 
-    // const newBook = await book.save();
-    //     //req.redirect(`books/${newBook.id}`)
-    //     req.redirect('books');
-
     try{
         const newBook = await book.save();
-        //req.redirect(`books/${newBook.id}`)
+        //res.redirect(`books/${newBook.id}`)
         res.redirect('books');
     }
     catch{
+        if(book.coverImageName != null){
+            removeBookCover(book.coverImageName);
+        }
+        
         renderNewPage(res, book, true);
 
     }
@@ -67,9 +90,14 @@ async function renderNewPage(res, book, hasError = false){
         res.render('books/new', params)
     }
     catch{
-        res.redirect('books/index')
+        res.redirect('books/index');
     }
 }
 
+function removeBookCover(fileName){
+    fs.unlink(path.join(uploadPath, fileName), err=>{
+        if(err) console.error(err);
+    });
+}
 
 module.exports = router;
